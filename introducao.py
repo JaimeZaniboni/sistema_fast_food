8742cc5f63b5e061e3ff411b01d8712ad5f36f86
JaimeZaniboni <124797927+JaimeZaniboni@users.noreply.github.com>
2023-02-08 11:08:02 -0300

    Add files via upload

---
+++ introducao.py
@@ -0,0 +1,8 @@
+n1 = float(input("digite o valor 1: "))
+n2 = float(input("digite o valor 2: "))
+
+print("soma : ", n1 + n2)
+print("subtração: ", n1 - n2)
+print("multiplicação: ", n1 * n2)
+print("divisão: ", n1 / n2)
+print("exponenciação: ", n1 ** n2)
